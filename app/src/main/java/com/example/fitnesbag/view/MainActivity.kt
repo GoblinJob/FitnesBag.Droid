994d2a33771a5bab1ea1beb2be561a3bcc5e9ca6
Mamed <boss.boss159@mail.ru>
2021-06-11 00:48:16 +0300

    Init project by libs

---
+++ app/src/main/java/com/example/fitnesbag/view/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.fitnesbag.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.fitnesbag.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
+}
