50f1683aa10af66989f1fef3fc31de8f2a4d51f7
Mamed <boss.boss159@mail.ru>
2021-06-11 22:54:27 +0300

    Main menu

--- app/src/main/java/com/example/fitnesbag/view/fragments/MainMenuFragment.kt
+++ app/src/main/java/com/example/fitnesbag/view/fragments/MainMenuFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.fitnesbag.R
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 class MainMenuFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main_menu, container, false)
+        val view = inflater.inflate(R.layout.fragment_main_menu, container, false)
+        val menu = view.findViewById<BottomNavigationView>(R.id.navigation_menu)
+        val navController = findNavController(R.id.fragment_menu)
+        return view
     }
 }
--- app/src/main/java/com/example/fitnesbag/view/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/example/fitnesbag/view/fragments/RegistrationFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.example.fitnesbag.R
+import com.example.fitnesbag.core.AuthViewModel
+import com.example.fitnesbag.core.RegistrationViewModel
+import com.example.fitnesbag.utils.ActionCallback
 import com.google.android.material.button.MaterialButton
 
 class RegistrationFragment : Fragment() {
+    lateinit var viewModel : RegistrationViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_registration, container, false)
+        viewModel = ViewModelProvider(this).get(RegistrationViewModel::class.java)
 
         setupRegistrationButton(view)
         
@@ -19,9 +26,12 @@ class RegistrationFragment : Fragment() {
     }
     
     private fun setupRegistrationButton(view: View) {
-        val openRegistrationFragment = view.findViewById<MaterialButton>(R.id.registrationButton)
-        openRegistrationFragment!!.setOnClickListener{
+        viewModel.setOnRegistrationAction(ActionCallback {
             NavHostFragment.findNavController(this).navigate(R.id.action_registrationFragment_to_mainMenuFragment)
+        })
+        val button = view.findViewById<MaterialButton>(R.id.registrationButton)
+        button!!.setOnClickListener{
+            viewModel.registration()
         }
     }
 }
